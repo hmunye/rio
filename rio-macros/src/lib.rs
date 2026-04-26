19da528af6807510a234a20909ec97d5a2e34403
Huseen <136820327+hmunye@users.noreply.github.com>
2026-04-26 11:43:49 -0400

    updated docs for proc macros

--- rio-macros/src/lib.rs
+++ rio-macros/src/lib.rs
@@ -4,21 +4,23 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{Attribute, ItemFn, parse_macro_input};
 
-/// Wraps the `main` function in a synchronous `rio` runtime entry point.
+/// Marks an `async` function as an entry point for a `rio` runtime.
 ///
-/// This macro allows you to define your program entry point using an
-/// `async fn main` signature, transforming this:
+/// This macro can be used on functions other than `main`. In that case, it
+/// creates a new runtime per-call and executes synchronously.
 ///
-/// ```text
+/// # Examples
+///
+/// ```rust,ignore
 /// #[rio::main]
 /// async fn main() {
 ///     // ...
 /// }
 /// ```
 ///
-/// Into a function equivalent to:
+/// This expands to code equivalent to:
 ///
-/// ```test
+/// ```rust,ignore
 /// fn main() {
 ///     let rt = rio::rt::Runtime::new();
 ///     rt.block_on(async {
@@ -32,21 +34,12 @@ pub fn main(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let sig = &input.sig;
 
     if sig.asyncness.is_none() {
-        return syn::Error::new_spanned(sig.fn_token, "must be used on an `async` function")
-            .to_compile_error()
-            .into();
-    }
-
-    if sig.ident != "main" {
-        return syn::Error::new_spanned(&sig.ident, "must be used on the `main` function")
-            .to_compile_error()
-            .into();
-    }
-
-    if !sig.inputs.is_empty() {
-        return syn::Error::new_spanned(&sig.inputs, "`main` function cannot accept arguments")
-            .to_compile_error()
-            .into();
+        return syn::Error::new_spanned(
+            sig.fn_token,
+            "`async` keyword is missing from the function declaration",
+        )
+        .to_compile_error()
+        .into();
     }
 
     let mut transformed = sig.clone();
@@ -65,14 +58,82 @@ pub fn main(_attr: TokenStream, item: TokenStream) -> TokenStream {
     })
 }
 
+/// Marks an `async` test function as an entry point for a `rio` runtime.
+///
+/// # Examples
+///
+/// ```rust,ignore
+/// #[rio::test]
+/// async fn my_async_test() {
+///     // ...
+/// }
+/// ```
+///
+/// This expands to code equivalent to:
+///
+/// ```rust,ignore
+/// #[test]
+/// fn my_async_test() {
+///     let rt = rio::rt::Runtime::new();
+///     rt.block_on(async {
+///         // ...
+///     })
+/// }
+/// ```
+#[proc_macro_attribute]
+pub fn test(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as ItemFn);
+
+    if let Some(attr) = input.attrs.iter().find(|attr| is_test_attribute(attr)) {
+        return syn::Error::new_spanned(
+            attr,
+            "second test attribute is supplied, consider removing or changing the order of your test attributes"
+        )
+        .to_compile_error()
+        .into();
+    }
+
+    let sig = &input.sig;
+
+    if sig.asyncness.is_none() {
+        return syn::Error::new_spanned(
+            sig.fn_token,
+            "`async` keyword is missing from the function declaration",
+        )
+        .to_compile_error()
+        .into();
+    }
+
+    if !sig.inputs.is_empty() {
+        return syn::Error::new_spanned(&sig.inputs, "test functions cannot accept arguments")
+            .to_compile_error()
+            .into();
+    }
+
+    let mut transformed = sig.clone();
+    transformed.asyncness = None;
+
+    let attrs = &input.attrs;
+    let vis = &input.vis;
+    let block = &input.block;
+
+    TokenStream::from(quote! {
+        #(#attrs)*
+        #[test]
+        #vis #transformed {
+            let rt = rio::rt::Runtime::new();
+            rt.block_on(async #block)
+        }
+    })
+}
+
 /// Check whether given attribute is a test attribute of forms:
 ///
 /// * `#[test]`
 /// * `#[core::prelude::*::test]` or `#[::core::prelude::*::test]`
 /// * `#[std::prelude::*::test]` or `#[::std::prelude::*::test]`
-///
-// <https://docs.rs/tokio-macros/2.6.1/src/tokio_macros/entry.rs.html#610>
 fn is_test_attribute(attr: &Attribute) -> bool {
+    // <https://docs.rs/tokio-macros/2.6.1/src/tokio_macros/entry.rs.html#610>
     let path = match &attr.meta {
         syn::Meta::Path(path) => path,
         _ => return false,
@@ -97,71 +158,5 @@ fn is_test_attribute(attr: &Attribute) -> bool {
         path.segments.iter().zip(segments).all(|(segment, path)| {
             segment.arguments.is_none() && (path == "*" || segment.ident == path)
         })
-    })
-}
-
-/// Wraps an `async` unit test in a synchronous `rio` runtime entry point.
-///
-/// Transforms this:
-///
-/// ```text
-/// #[rio::test]
-/// async fn my_async_test() {
-///     // ...
-/// }
-/// ```
-///
-/// Into a function equivalent to:
-///
-/// ```text
-/// #[test]
-/// fn my_async_test() {
-///     let rt = rio::rt::Runtime::new();
-///     rt.block_on(async {
-///         // ...
-///     })
-/// }
-/// ```
-#[proc_macro_attribute]
-pub fn test(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(item as ItemFn);
-
-    if let Some(attr) = input.attrs.iter().find(|attr| is_test_attribute(attr)) {
-        return syn::Error::new_spanned(
-            attr,
-            "second test attribute supplied, consider removing or changing the order of your test attributes"
-        )
-        .to_compile_error()
-        .into();
-    }
-
-    let sig = &input.sig;
-
-    if sig.asyncness.is_none() {
-        return syn::Error::new_spanned(sig.fn_token, "must be used on an `async` function")
-            .to_compile_error()
-            .into();
-    }
-
-    if !sig.inputs.is_empty() {
-        return syn::Error::new_spanned(&sig.inputs, "test functions cannot accept arguments")
-            .to_compile_error()
-            .into();
-    }
-
-    let mut transformed = sig.clone();
-    transformed.asyncness = None;
-
-    let attrs = &input.attrs;
-    let vis = &input.vis;
-    let block = &input.block;
-
-    TokenStream::from(quote! {
-        #(#attrs)*
-        #[test]
-        #vis #transformed {
-            let rt = rio::rt::Runtime::new();
-            rt.block_on(async #block)
-        }
     })
 }
